ecacfc37220be65032fb8cf596e17e903f8312fe
Joao Carlos Moura <jmoura@delo99.com.br>
2018-07-03 17:43:35 -0300

    Criação do projeto

---
+++ src/main/java/br/com/delo99/useapi/UseapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.delo99.useapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseapiApplication.class, args);
+	}
+}
